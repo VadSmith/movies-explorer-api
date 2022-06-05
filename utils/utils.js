97be42fb338b194249936a2b6a9a378e2b85f81f
VadSmith <oldfatham@gmail.com>
2022-06-05 21:20:56 +0700

    fix eslint arrow errors

--- utils/utils.js
+++ utils/utils.js
@@ -1,6 +1,8 @@
-// eslint-disable-next-line consistent-return
 const isObjectIdExists = (MongoModel, ObjectId) => {
-  if (MongoModel.findById(ObjectId).count() > 0) return true;
+  if (MongoModel.findById(ObjectId).count() > 0) {
+    return true;
+  }
+  return false;
 };
 
 const urlRegexp = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w.-]+)+[\w\-._~:/?#[\]@!$&'()*+,;=.]+$/;
